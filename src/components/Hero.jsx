186a847318ae3189840632cc67686b7803ef58df
markhadi <markhadi@protonmail.com>
2023-09-18 17:39:44 +0800

    create navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex flex-col justify-center items-center ">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/markhadi")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Hero;
